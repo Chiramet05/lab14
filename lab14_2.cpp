09165f27d300df67957fe386c1e5bda319479820
Chiramert <fortjiramet10@gmail.com>
2025-01-28 00:16:25 +0700

    lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,51 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+//Do not modify source code above this line
+void inputMatrix(double A[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Row " << i + 1 << ": ";
+		for (int j = 0; j < N; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+}
+void showMatrix(const bool B[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << B[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void findLocalMax(const double A[][N], bool B[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			B[i][j] = 0;
+		}
+	}
+	for (int i = 1; i < N - 1; i++)
+	{
+		for (int j = 1; j < N - 1; j++)
+		{
+			double current = A[i][j];
+			double up = A[i - 1][j];
+			double down = A[i + 1][j];
+			double left = A[i][j - 1];
+			double right = A[i][j + 1];
+			if (current >= up && current >= down && current >= left && current >= right)
+			{
+				B[i][j] = 1;
+			}
+		}
+	}
+}
